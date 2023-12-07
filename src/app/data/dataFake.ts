de3cf834064426a5f9d1b4f982f077a52f726dce
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2023-12-07 16:01:00 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://www.infomoney.com.br/wp-content/uploads/2019/06/acoes-queda-baixa-bear.jpg?resize=900%2C515&quality=70&strip=all",
+    "title": "Petrobras em QUEDA",
+    "description": "Ações da petroleira caem mais de 4% com forte baixa da commodity"
+  },
+  {
+    "id": "2",
+    "photo": "https://s2-g1.glbimg.com/RXPEyVq3EGvBiPszOouvKQCWB9g=/0x0:5616x3744/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/A/P/eAGVvrSECcxMxn2HMfRw/foto-02-real-invest-materia-03.jpg",
+    "title": "Dolar em alta",
+    "description": "O Dolar opera em alta nas expectativas do payroll que ocorre sexta-feira"
+  },
+  {
+    "id": "3",
+    "photo": "https://marketinsider.com.br/wp-content/uploads/2023/12/vale-acoes-bolsa-jpg.webp",
+    "title": "Vale (VALE3) DECOLANDO",
+    "description": "As ações da Vale amanheceram em alta em meio a valorização do minério"
+  },
+  {
+    "id": "4",
+    "photo": "https://www.infomoney.com.br/wp-content/uploads/2021/12/GettyImages-1339662679-2.jpg?fit=789%2C442&quality=70&strip=all",
+    "title": "Bitcoin: O que esperar",
+    "description": "Após um grande rali, o Bitcoin perde forças e rompe um importante suporte de U$44 mil"
+  },
+]
